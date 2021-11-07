35a06a24801c3fddfac2ef99d7fe68758fa33daf
alexmpc98 <48052322+alexmpc98@users.noreply.github.com>
2021-11-07 22:16:32 +0000

    Lab 3 - Nivel 3 (1/2)
    Lab 3 - Nivel 3 (1/2)

---
+++ 3º Ano/Anual/Programação Visual (PV)/Labs/Lab 3/CineSetúbal/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using CineSetúbal.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CineSetúbal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private List<Filme> filmes;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            filmes = new List<Filme> {
+                 new Filme
+                 {
+                     Titulo = "Pulp Fiction",
+                     Diretor = "Quentin Tarantino",
+                     Atores = new List<string>{"John Travolta", "Uma Thurman", "Samuel L. Jackson" },
+                     Duracao = 154,
+                     Ano = 1994,
+                     Estreia = true
+                 },
+                 new Filme
+                 {
+                     Titulo = "Blade Runner",
+                     Diretor = "Ridley Scott",
+                     Atores = new List<string>{"Harrison Ford", "David Webb Peoples", "Philip K. Dick" },
+                     Duracao = 117,
+                     Ano = 1982,
+                     Estreia = false
+                 },
+                 new Filme
+                 {
+                     Titulo = "Friday the 13th",
+                     Diretor = "Sean S. Cunningham Leone",
+                     Atores = new List<string>{"Betsy Palmer", "Adrienne King", "Jeannine Taylor" },
+                     Duracao = 95,
+                     Ano = 1980,
+                     Estreia = false
+                 },
+                 new Filme
+                 {
+                     Titulo = "The Good, The Bad & The Ugly",
+                     Diretor = "Sergio Leone",
+                     Atores = new List<string>{"Clint Eastwood", "Eli Wallach", "Lee Van Cleef" },
+                     Duracao = 161,
+                     Ano = 1966,
+                     Estreia = true
+                 }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult FilmeDoDia()
+        {
+            Filme movie = filmes[0];
+            int movieIndex = filmes.FindIndex(a => a.Equals(movie)) + 1;
+            ViewData["IndexFilme"] = "Filme " +  movieIndex.ToString() + " de " + filmes.Count().ToString();   
+            return View(movie);
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 3º Ano/Anual/Programação Visual (PV)/Labs/Lab 3/CineSetúbal/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CineSetúbal
+{
+    public class Filme
+    {
+        public string Titulo { get; set; }
+        public string Diretor { get; set; }
+        public List<String> Atores { get;set; }
+        public int Duracao { get; set; }
+        public int Ano { get; set; }
+        public bool Estreia { get; set; }
+    }
+}
